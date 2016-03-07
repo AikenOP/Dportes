61b150559c3e4b210555dbcb32aa51dd4e97d0ca
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2016-03-07 12:44:43 -0300

    ajuste para notificación PUSH

--- www/js/pageinit.js
+++ www/js/pageinit.js
@@ -29,7 +29,7 @@
                         },
                         windows: {}
                     });
-
+                    alert('working');
                     push.on('registration', function(data) {
                         // data.registrationId
                     });
