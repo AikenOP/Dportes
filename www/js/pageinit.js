9bda8299821754f0cb246b743d6ec7aae52799ad
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2016-05-12 10:46:20 -0300

    push get

--- www/js/pageinit.js
+++ www/js/pageinit.js
@@ -64,7 +64,7 @@
                 });
 
                 push.on('registration', function(data) {
-                    alert(data);
+                    alert(data.registrationId);
                     var reg = new registro();
                     reg.key = data.registrationId;
                     reg.setKeyByUsuario();
