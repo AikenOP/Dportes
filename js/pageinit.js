38f6e4a36411f21999ceb6e2e709c3a61133adc9
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2016-06-09 11:58:05 -0400

    fix

--- js/pageinit.js
+++ js/pageinit.js
@@ -100,8 +100,8 @@
         }
 
         if(activePage === 'cambio-clave'){
-            document.getElementById('conf-password').value('');
-            document.getElementById('conf-matches').value('');
+            document.getElementById('conf-password').value = '';
+            document.getElementById('conf-matches').value = '';
             document.getElementById('conf-error-pass').style.display = "none";
             document.getElementById('conf-error-matches').style.display = "none";
             document.getElementById('conf-correcto').style.display = "none";
