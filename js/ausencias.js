daca156434fb62640007dc9ddbe7ee89501b81c1
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2016-06-10 12:04:34 -0400

    set ausencia

--- js/ausencias.js
+++ js/ausencias.js
@@ -4,10 +4,11 @@ function ausencias(){
 	this.nombre
 	this.descripcion
 	this.foto
+    this.id_notificacion
+    this.id_usuario
 
 
 	this.getTiposAusencias = function(){
-        alert();
 		var xhr = new XMLHttpRequest();
 		xhr.open('POST', path + 'app/getTiposAusencias');
 	    xhr.setRequestHeader('Cache-Control', 'no-cache');
@@ -41,11 +42,40 @@ function ausencias(){
             }
         }
 	}
+
+    this.setAusencia = function(){
+        var xhr = new XMLHttpRequest();
+        var add = new FormData();
+        add.append('id_tipo',this.id_tipo);
+        add.append('id_notificacion',this.id_notificacion);
+        add.append('id_usuario',this.id_usuario);
+        xhr.open('POST', path + 'app/setAusencia');
+        xhr.setRequestHeader('Cache-Control', 'no-cache');
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        xhr.send(add);
+        xhr.timeout = 10000;
+        xhr.onprogress = function(e){
+            $.mobile.loading('show');
+        }
+        xhr.ontimeout = function(e){
+            navigator.notification.alert('Se detecto un problema, intentelo nuevamente',function(){},'Atención','OK');   
+        }
+        xhr.onload = function(e){
+            $.mobile.loading('hide');
+            if(this.status == 200){
+                $.mobile.navigate("#asistencias", {transition: "fade"});    
+            }
+        }
+    }
 }
 
 
 function setAusencia(id,notifica,usuario){
-    alert(id);
-    alert(notifica);
-    alert(usuario);
+    var ausencia = new ausencias();
+    ausencia.id_tipo = id;
+    ausencia.id_notificacion = notifica;
+    ausencia.id_usuario = usuario;
+    ausencia.setAusencia();
+    delete ausencia;
+
 }
