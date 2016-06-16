27da28f3ae4681c1551f796d5e251bd819c2ed83
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2016-06-16 15:18:19 -0400

    fix

--- js/pageinit.js
+++ js/pageinit.js
@@ -35,8 +35,8 @@
                 $('#nav-jg').removeClass('ui-state-disabled');
                 $('#home-stat').removeClass('ui-state-disabled');
                 $('#nav-stat').removeClass('ui-state-disabled');
-                var jugadores = new jugadores();
-                jugadores.setRolEquipo();
+                var jg = new jugadores();
+                jg.setRolEquipo();
             } else {
                 $('#home-add-jg').addClass('ui-state-disabled');
                 $('#nav-jg').addClass('ui-state-disabled');
