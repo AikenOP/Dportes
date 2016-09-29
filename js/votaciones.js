cbc85c7d3a421c6afdd0d7b3a43daf9442aeb3c8
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2016-09-29 10:35:01 -0300

    alerta

--- js/votaciones.js
+++ js/votaciones.js
@@ -33,6 +33,7 @@ function votaciones(){
             navigator.notification.alert('Se detecto un problema, intentelo nuevamente',function(){},'Atención','OK');
         };
         xhr.onload = function(e){
+            alert(this.response);
         	$.mobile.loading('hide');
         	if(this.status == 200){
         		if(this.response && JSON.parse(this.response)){
