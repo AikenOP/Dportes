bf81b8372857ccadbfbd0545aaeb2d9498c578f8
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2016-10-18 15:41:31 -0300

    prueba contacto

--- js/eventos.js
+++ js/eventos.js
@@ -562,8 +562,27 @@ document.getElementById('stat-extra').addEventListener('click',function(){
 });
 
 document.getElementById('pg-contact').addEventListener('click',function(){
-    alert('contact');
+    var options = new ContactFindOptions();
+    options.filter = "";
+    options.multiple = true;
+    filter = ["displayName", "name"];
+    navigator.contacts.find(filter, onSuccess, onError, options);
 });
+
+function onSuccess(contacts) {
+    for (var i = 0; i < contacts.length; i++) {
+        alert("Formatted: "  + contacts[i].name.formatted       + "\n" +
+            "Family Name: "  + contacts[i].name.familyName      + "\n" +
+            "Given Name: "   + contacts[i].name.givenName       + "\n" +
+            "Middle Name: "  + contacts[i].name.middleName      + "\n" +
+            "Suffix: "       + contacts[i].name.honorificSuffix + "\n" +
+            "Prefix: "       + contacts[i].name.honorificSuffix);
+    }
+};
+
+function onError(contactError) {
+    alert('onError!');
+};
 
 
 function checkProgramados(){
