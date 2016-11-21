bc923981da3cb551229a031378013e423776e011
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2016-11-21 11:55:58 -0300

    error

--- js/contactos.js
+++ js/contactos.js
@@ -2,24 +2,25 @@ function contactos(){
 
 
 	this.getContacts = function(){
-		alert('ere');
-		try{
 	    var options = new ContactFindOptions();
 	    options.filter = "";
 	    options.multiple = true;
 	    filter = ["displayName", "name"];
-	    navigator.contacts.find(filter, function(){
-		    for (var i = 0; i < contacts.length; i++) {
-		        alert("Formatted: "  + contacts[i].name.formatted       + "\n" +
-		            "Family Name: "  + contacts[i].name.familyName      + "\n" +
-		            "Given Name: "   + contacts[i].name.givenName       + "\n" +
-		            "Middle Name: "  + contacts[i].name.middleName      + "\n" +
-		            "Number: "       + contacts[i].phoneNumbers[0].value + "\n" +
-		            "Prefix: "       + contacts[i].name.honorificSuffix);
-		    }
-	    }, function(){alert('onError!');}, options);
-		} catch(e){
-			alert(e);
-		}
+	    navigator.contacts.find(filter, onSuccess, onError, options);
 	}
 }
+
+	function onSuccess(){
+	    for (var i = 0; i < contacts.length; i++) {
+	        alert("Formatted: "  + contacts[i].name.formatted       + "\n" +
+	            "Family Name: "  + contacts[i].name.familyName      + "\n" +
+	            "Given Name: "   + contacts[i].name.givenName       + "\n" +
+	            "Middle Name: "  + contacts[i].name.middleName      + "\n" +
+	            "Number: "       + contacts[i].phoneNumbers[0].value + "\n" +
+	            "Prefix: "       + contacts[i].name.honorificSuffix);
+	    }
+	}
+
+	function onError(){
+		alert('error');
+	}
