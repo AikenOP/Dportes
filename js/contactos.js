8178a397cbb7aa9ef7bc69612a6ae5cc756bfeca
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2016-11-21 11:42:17 -0300

    change

--- js/contactos.js
+++ js/contactos.js
@@ -6,21 +6,15 @@ function contactos(){
 	    options.filter = "";
 	    options.multiple = true;
 	    filter = ["displayName", "name"];
-	    navigator.contacts.find(filter, this.onSuccess, this.onError, options);
-	}
-
-	this.onSuccess = function(){
-	    for (var i = 0; i < contacts.length; i++) {
-	        alert("Formatted: "  + contacts[i].name.formatted       + "\n" +
-	            "Family Name: "  + contacts[i].name.familyName      + "\n" +
-	            "Given Name: "   + contacts[i].name.givenName       + "\n" +
-	            "Middle Name: "  + contacts[i].name.middleName      + "\n" +
-	            "Number: "       + contacts[i].phoneNumbers[0].value + "\n" +
-	            "Prefix: "       + contacts[i].name.honorificSuffix);
-	    }
-	}
-
-	this.onError = function(){
-		alert('onError!');
+	    navigator.contacts.find(filter, function(){
+		    for (var i = 0; i < contacts.length; i++) {
+		        alert("Formatted: "  + contacts[i].name.formatted       + "\n" +
+		            "Family Name: "  + contacts[i].name.familyName      + "\n" +
+		            "Given Name: "   + contacts[i].name.givenName       + "\n" +
+		            "Middle Name: "  + contacts[i].name.middleName      + "\n" +
+		            "Number: "       + contacts[i].phoneNumbers[0].value + "\n" +
+		            "Prefix: "       + contacts[i].name.honorificSuffix);
+		    }
+	    }, function(){alert('onError!');}, options);
 	}
 }
