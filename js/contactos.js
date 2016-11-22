63a64654ec1b12a77e09ad058199de806c2f9fb5
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2016-11-22 11:46:48 -0300

    contactos

--- js/contactos.js
+++ js/contactos.js
@@ -12,12 +12,12 @@ function contactos(){
 
 	this.onSuccess = function(contacts){
 	    for (var i = 0; i < contacts.length; i++) {
-	        alert("Formatted: "  + contacts[i].name.formatted       + "\n" +
+	        /*alert("Formatted: "  + contacts[i].name.formatted       + "\n" +
 	            "Family Name: "  + contacts[i].name.familyName      + "\n" +
 	            "Given Name: "   + contacts[i].name.givenName       + "\n" +
 	            "Middle Name: "  + contacts[i].name.middleName      + "\n" +
 	            "Number: "       + contacts[i].phoneNumbers[0].value + "\n" +
-	            "Prefix: "       + contacts[i].name.honorificSuffix);
+	            "Prefix: "       + contacts[i].name.honorificSuffix);*/
 	    }		
 	}
 
