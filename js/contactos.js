f1a64360b1d1f8824c916cacf5464d002299c488
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2016-11-22 11:57:19 -0300

    list

--- js/contactos.js
+++ js/contactos.js
@@ -11,14 +11,23 @@ function contactos(){
 
 
 	this.onSuccess = function(contacts){
+		var inc = '';
+		var logo = '';
 	    for (var i = 0; i < contacts.length; i++) {
+	    	inc += "<li data-icon='false' class='li-padding'>";
+           	inc += "<label>";
+            inc += "<div class='imagen_jugador'><img src='jquerymobile/img-dportes/foto.png'></div>";
+            inc += "<h2>"+contacts[i].name.formatted+"</h2>";
+            inc += "</li>";
 	        /*alert("Formatted: "  + contacts[i].name.formatted       + "\n" +
 	            "Family Name: "  + contacts[i].name.familyName      + "\n" +
 	            "Given Name: "   + contacts[i].name.givenName       + "\n" +
 	            "Middle Name: "  + contacts[i].name.middleName      + "\n" +
 	            "Number: "       + contacts[i].phoneNumbers[0].value + "\n" +
 	            "Prefix: "       + contacts[i].name.honorificSuffix);*/
-	    }		
+	    }
+	    $('#contactos-list').html(inc).listview('refresh');
+	    		
 	}
 
 	this.onError = function(contactError){
