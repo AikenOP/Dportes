de6c3d481e2c12f6fe0fdb3fd0bf84372769c2d5
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2016-12-16 15:23:59 -0300

    boton

--- js/contactos.js
+++ js/contactos.js
@@ -11,7 +11,7 @@ function contactos(){
 
 
 	this.onSuccess = function(contacts){
-		alert(JSON.stringify(contacts));
+		//alert(JSON.stringify(contacts));
 		var inc = '';
 		var logo = '';
 	    for (var i = 0; i < contacts.length; i++) {
