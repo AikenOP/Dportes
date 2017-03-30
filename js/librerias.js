a80d40d65e2b8dd96752f1ce9e1f1daf656b390b
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2017-03-30 10:43:43 -0300

    cambios

--- js/librerias.js
+++ js/librerias.js
@@ -183,6 +183,37 @@ function countdown(id,fecha){
     }
 }
 
+
+function cronometro(){
+    var tiempo = {
+        hora: 0,
+        minuto: 0,
+        segundo: 0
+    };
+
+    var tiempo_corriendo = null;                      
+            tiempo_corriendo = setInterval(function(){
+                // Segundos
+                tiempo.segundo++;
+                if(tiempo.segundo >= 60)
+                {
+                    tiempo.segundo = 0;
+                    tiempo.minuto++;
+                }      
+
+                // Minutos
+                if(tiempo.minuto >= 60)
+                {
+                    tiempo.minuto = 0;
+                    tiempo.hora++;
+                }
+
+                $("#hour").text(tiempo.hora < 10 ? '0' + tiempo.hora : tiempo.hora);
+                $("#minute").text(tiempo.minuto < 10 ? '0' + tiempo.minuto : tiempo.minuto);
+                $("#second").text(tiempo.segundo < 10 ? '0' + tiempo.segundo : tiempo.segundo);
+            }, 1000);
+}
+
 function getHora(hora){
 	var hor = hora.split(" ");
 	var arr = hor[1].split(":");
